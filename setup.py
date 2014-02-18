641658e7dd4aaf7a22375996efe599444bd7858f
Kouhei Maeda <mkouhei@palmtb.net>
2014-02-19 07:31:26 +0900

    Fix invalid pep 8 style guide.
    Signed-off-by: Kouhei Maeda <mkouhei@palmtb.net>

--- setup.py
+++ setup.py
@@ -16,16 +16,18 @@ classifiers = [
 ]
 
 setup(
-    name = "shortuuid",
-    version = "0.3.1",
-    author = "Stochastic Technologies",
-    author_email = "info@stochastictechnologies.com",
-    url = "https://github.com/stochastic-technologies/shortuuid/",
-	description = "A generator library for concise, unambiguous and URL-safe UUIDs.",
-	long_description = "A library that generates short, pretty, unambiguous unique IDs "
-                       " by using an extensive, case-sensitive alphabet and omitting "
-                       "similar-looking letters and numbers.",
-	license = "BSD",
+    name="shortuuid",
+    version="0.3.1",
+    author="Stochastic Technologies",
+    author_email="info@stochastictechnologies.com",
+    url="https://github.com/stochastic-technologies/shortuuid/",
+    description="A generator library for concise, "
+    "unambiguous and URL-safe UUIDs.",
+    long_description="A library that generates short, pretty, "
+    "unambiguous unique IDs "
+    " by using an extensive, case-sensitive alphabet and omitting "
+    "similar-looking letters and numbers.",
+    license="BSD",
     classifiers=classifiers,
-    packages = ["shortuuid"],
+    packages=["shortuuid"],
 )
