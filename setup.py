cf91791fadbf7dc00bc8f430631bf2cccb7f163f
Stavros Korokithakis <stavros@korokithakis.net>
2017-06-15 15:09:37 +0300

    test: Add more Python versions to the tests

--- setup.py
+++ setup.py
@@ -11,8 +11,10 @@ classifiers = [
     "Programming Language :: Python :: 2.5",
     "Programming Language :: Python :: 2.6",
     "Programming Language :: Python :: 2.7",
-    "Programming Language :: Python :: 3.2",
     "Programming Language :: Python :: 3.3",
+    "Programming Language :: Python :: 3.4",
+    "Programming Language :: Python :: 3.5",
+    "Programming Language :: Python :: 3.6",
     "Topic :: Software Development :: Libraries :: Python Modules",
 ]
 
