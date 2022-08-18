854c4d6a75b6b3505f1e0d192450115fbc93efe4
tim <tcrothers123@gmail.com>
2022-08-18 20:25:37 +0100

    feat: use argparse in cli
    currently just adds a nice help message and
    otherwise maintains current functionality

--- shortuuid/cli.py
+++ shortuuid/cli.py
@@ -1,5 +1,16 @@
+import argparse
+import sys
+from typing import Any
+
 from .main import uuid
 
 
-def main() -> None:
+def cli(*args: Any) -> None:
+    parser = argparse.ArgumentParser(description='Print a random shortuuid')
+    parser.parse_args(*args)
+
     print(uuid())
+
+
+def main() -> None:
+    cli(sys.argv[1:])
