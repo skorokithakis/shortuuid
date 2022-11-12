431c88c9677354945e3e6d50f28b257d3e1ef9bf
Nicholas Bollweg <nick.bollweg@gmail.com>
2022-11-12 09:02:34 -0600

    explicitly re-export imported name

--- shortuuid/__init__.py
+++ shortuuid/__init__.py
@@ -1,9 +1,12 @@
-from shortuuid.main import decode  # noqa
-from shortuuid.main import encode  # noqa
-from shortuuid.main import get_alphabet  # noqa
-from shortuuid.main import random  # noqa
-from shortuuid.main import set_alphabet  # noqa
-from shortuuid.main import ShortUUID  # noqa
-from shortuuid.main import uuid  # noqa
+from shortuuid.main import decode
+from shortuuid.main import encode
+from shortuuid.main import get_alphabet
+from shortuuid.main import random
+from shortuuid.main import set_alphabet
+from shortuuid.main import ShortUUID
+from shortuuid.main import uuid
 
 __version__ = "1.0.10"
+__all__ = [
+  "decode", "encode", "get_alphabet", "random", "set_alphabet", "ShortUUID", "uuid"
+]
